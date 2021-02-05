5bfbb052536cf837dfc95c3e3016bc315107c354
Rob Scheirlinckx <r0783432@student.vives.be>
2021-02-05 12:51:04 +0100

    tweede commit

--- groepen.js
+++ groepen.js
@@ -1,8 +1,26 @@
-const groep = () => {
-    let datum = 99999999999999999
+const groepV1 = () => {
+    console.log("************************************** GroepenV1 **************************************")
+    let datum = 20201010
     console.log("Geef je geboortedatum in (JJJJMMDD): " + datum);
-    let groep = datum%7+1;
+    let groep = datum%7;
     console.log("Je bent toegewezen aan groep " + groep);
+    console.log("***************************************************************************************")
 }
 
-groep();
+
+
+groepV1();
+
+const readline = require("readline");
+    const r_datum = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+r_datum.question("Geef je geboortedatum in (JJJJMMDD): ", function(gbDatum) {
+    console.log("************************************** GroepenV2 **************************************")
+    let groep = gbDatum%7;
+    console.log("Je bent toegewezen aan groep " + groep);
+    console.log("***************************************************************************************")
+    r_datum.close();
+})
